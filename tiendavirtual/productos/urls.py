6e4e2d5bf55a8266abd6dfaa97b6364c579bb911
kevincz <k.castaneda@znerg.com>
2016-07-02 00:01:00 -0500

    categorias

--- tiendavirtual/productos/urls.py
+++ tiendavirtual/productos/urls.py
@@ -3,4 +3,5 @@ from productos import views
 
 urlpatterns = [
     url(r'^catalogo/$', views.catalogo, name='catalogo'),
+    url(r'^catalogo/categoria/(?P<id_categoria>[0-9]+)/$', views.catalogo_categoria, name='catalogo_categoria'),
 ]
--- tiendavirtual/productos/views.py
+++ tiendavirtual/productos/views.py
@@ -1,6 +1,6 @@
 from django.template import loader
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Producto,Categoria,Subcategoria
 # Create your views here.
 
@@ -14,3 +14,16 @@ def catalogo(request):
                'categorias': c,
                'subcategorias':s,
                })
+
+
+def catalogo_categoria(request, id_categoria):
+
+    categoria = get_object_or_404(Categoria, id=id_categoria)
+    p = Producto.objects.filter(categoria = categoria.id)
+    c = Categoria.objects.all()
+    s = Subcategoria.objects.all()
+
+    return render(request, "presentation/products.html", {'productos': p,
+                                                          'categorias': c,
+                                                          'subcategorias': s,
+                                                          })
